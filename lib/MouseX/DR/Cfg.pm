2655872b5dfb2db2d8365927091b682b1c7ce12f
Dmitry E. Oboukhov <unera@debian.org>
2018-10-15 12:17:40 +0300

    Prototype for perldoc.

--- lib/MouseX/DR/Cfg.pm
+++ lib/MouseX/DR/Cfg.pm
@@ -2,6 +2,8 @@ package MouseX::DR::Cfg;
 use Mouse;
 extends 'Mouse::Meta::Attribute';
 
+our $VERSION = '0.02';
+
 has cfg    =>
     is          => 'ro',
     isa         => 'HashRef',
@@ -51,3 +53,44 @@ around cfg => sub {
 
 no Mouse;
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
+
+__END__
+
+=head1 NAME
+
+MouseX::DR::Cfg - Config tags for any meta attributes
+
+=head1 SYNOPSIS
+
+    package Foo;
+    use Mouse;
+    use MouseX::DR::Cfg;
+
+    has foo     => is => 'ro', cfg => { jsonable => 1 };
+    has bar     => is => 'ro', cfg => { jsonable => 0 };
+    has baz     => is => 'ro', cfg => { jsonable => 1 };
+
+
+    sub TO_JSON {
+        my ($self) = @_;
+        my %h;
+        for ($self->get_attribute_list) {
+            next unless $self->get_attribute($_)->cfg('jsonable');
+            $h{ $_ } = $self->get_attribute($_);
+        }
+        return \%h;
+    }
+
+    __PACKAGE__->meta->make_immutable;
+
+=head1 DESCRIPTION
+
+The package replaces standard class L<Mouse::Meta::Attribute>
+by L<MouseX::DR::Cfg>, so each attribute receives a hash B<cfg>.
+
+You can access the hash through C<cfg> attribute.
+
+    $self->get_attribute($name)->cfg->{a}{b}[3]{c};
+    $self->get_attribute($name)->cfg('a.b.3.c');
+
+=cut
